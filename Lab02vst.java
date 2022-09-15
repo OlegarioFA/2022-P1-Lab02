003ff8f56a7aa16d0a95fb609fe202932ac954e0
Olegario <olegario.fernandezalonso@vtsd.com>
2022-09-15 08:06:50 -0400

    Code Update Upload

--- Lab02vst.java
+++ Lab02vst.java
@@ -6,22 +6,22 @@
 
 */
 
-  public class Lab02vst {
+public class Lab02vst {
 
-    public static void main(String args[]){
-      System.out.println("Lab 02, 100 Point Version");
-      System.out.println("");
-      System.out.println("");
-      System.out.print("Measure today's knowledge ");
-      System.out.println("by yesterday's confusion.");
-      System.out.println("");
-      System.out.println("Bewilderment + Exposure = Obvious");
-      System.out.println("");
-      System.out.println("#  #####  #####     #####  #####  #  #    #  #####  #####");
-      System.out.println("#  #   #  #   #     #   #  #   #  #  ##   #    #    #    ");
-      System.out.println("#  #   #  #   #     #####  #   #  #  # #  #    #    #####");
-      System.out.println("#  #   #  #   #     #      #   #  #  #  # #    #        #");
-      System.out.println("#  #####  #####     #      #####  #  #   ##    #    #####");
+    public static void main(String args[]) {
+        System.out.println("Lab 02, 100 Point Version");
+        System.out.println("");
+        System.out.println("");
+        System.out.print("Measure today's knowledge ");
+        System.out.println("by yesterday's confusion.");
+        System.out.println("");
+        System.out.println("Bewilderment + Exposure = Obvious");
+        System.out.println("");
+        System.out.println("#  #####  #####     #####  #####  #  #    #  #####  #####");
+        System.out.println("#  #   #  #   #     #   #  #   #  #  ##   #    #    #    ");
+        System.out.println("#  #   #  #   #     #####  #   #  #  # #  #    #    #####");
+        System.out.println("#  #   #  #   #     #      #   #  #  #  # #    #        #");
+        System.out.println("#  #####  #####     #      #####  #  #   ##    #    #####");
 
     }
-  }
+}
